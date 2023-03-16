a11408e8c4dd342809a8aea021068dc48eb5766a
Zara Martín <zmartand@myuax.es>
2023-03-16 20:18:30 +0100

    apartado b terminado

--- src/main/java/org/example/Funcion.java
+++ src/main/java/org/example/Funcion.java
@@ -10,6 +10,22 @@ public class Funcion {
             resultado = x * multiplicacionSucesiva(x, y - 1); //Multiplicamos la base(x) por si misma la cantidad de veces que sea la potencia(y).
         }
         return resultado; //Imprimimos el resultado :)
-    }
+    } //Recursividad de cola
+
+    //apartado b)
+    public double multiplicacionSucesiva2(double x, double y) {
+        double resultado = 0;
+        if (y == 0) {
+            return 1;
+        } else {
+            if (y % 2 == 0) { //Si la potencia es par
+                resultado = multiplicacionSucesiva2(x, y / 2) * multiplicacionSucesiva2(x, y / 2); //Multiplicamos la base(x) por si misma la cantidad de veces que sea la potencia(y) / 2.
+            } else {
+                resultado = x * multiplicacionSucesiva2(x, y - 1); //idem y-1
+            }
+        }
+        return resultado; //Imprimimos el resultado
+    } //Recursividad de división y conquista
+
 }
 
