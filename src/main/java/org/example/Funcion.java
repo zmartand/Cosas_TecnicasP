f975787ef5607b6e958d875d1bff2494356e7ec2
Zara Martín <zmartand@myuax.es>
2023-03-16 19:39:08 +0100

    intento apartado a

--- src/main/java/org/example/Funcion.java
+++ src/main/java/org/example/Funcion.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Funcion {
+    //apartado a
+    public double multiplicacionSucesiva(double x, double y) {
+        double resultado = 0;
+        int count = 0;
+        do {
+            for (int i = 0; i < y; i++) {
+                resultado = Math.pow(x, y);
+                count++;
+            }
+
+        } while (count != y);
+        return resultado;
+    }
 }
+
