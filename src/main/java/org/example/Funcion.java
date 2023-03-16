b95a7239125f2fc33868e3c70d6e7986c9374552
Zara Martín <zmartand@myuax.es>
2023-03-16 19:51:08 +0100

    apartado a terminado

--- src/main/java/org/example/Funcion.java
+++ src/main/java/org/example/Funcion.java
@@ -1,18 +1,15 @@
 package org.example;
 
 public class Funcion {
-    //apartado a
+    //apartado a)
     public double multiplicacionSucesiva(double x, double y) {
         double resultado = 0;
-        int count = 0;
-        do {
-            for (int i = 0; i < y; i++) {
-                resultado = Math.pow(x, y);
-                count++;
-            }
-
-        } while (count != y);
-        return resultado;
+        if (y == 0) {
+            return 1; //Un numero elevado a 0 es 1
+        } else {
+            resultado = x * multiplicacionSucesiva(x, y - 1); //Multiplicamos la base(x) por si misma la cantidad de veces que sea la potencia(y).
+        }
+        return resultado; //Imprimimos el resultado :)
     }
 }
 
